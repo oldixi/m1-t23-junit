82048689ec88b9a31fa6a96497a2498ce8371309
oldixi <oldixi@yandex.ru>
2023-07-05 14:39:31 +0300

    tests: Добавить автотест ReversePolishNotationCalculatorTest для покрытия 100% кода метода ReversePolishNotationCalculator

--- test/notation/ReversePolishNotationCalculatorTest.java
+++ test/notation/ReversePolishNotationCalculatorTest.java
@@ -1,11 +1,14 @@
 package notation;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReversePolishNotationCalculatorTest {
+    ReversePolishNotationCalculator calculator = new ReversePolishNotationCalculator();
+    String strToCalc = "1 5 3 2  * - +";
 
     @Test
     public void shouldCalculateAddition() {
-
+        assertEquals(0, calculator.calculatePolishNotation(strToCalc));
     }
 }
